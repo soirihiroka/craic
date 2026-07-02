a0ee714878ba9598810b835d94c2c85702bfd26b
Soiri Hiroka <soirihiroka@users.noreply.github.com>
2026-07-02 17:15:37 +0000

    refactor: build shortcuts dialog from section data
    Replaced the large inline GtkBuilder XML for the keyboard shortcuts window with a data-driven `ShortcutsDialog` builder in `src/ui/shortcuts.rs`. Introduced `SHORTCUT_SECTIONS` as a constant list of section titles and `(label, accelerator)` pairs, then iterate to create `adw::ShortcutsSection` and `adw::ShortcutsItem` entries at runtime. Updated presentation to use `adw::ShortcutsDialog` directly with `present(Some(parent))`, and adjusted the log message wording from window to dialog.

--- src/ui/shortcuts.rs
+++ src/ui/shortcuts.rs
@@ -1,354 +1,103 @@
 use adw::prelude::*;
 
-pub(super) fn show_shortcuts_window(parent: &adw::ApplicationWindow) {
-    let xml = r#"<?xml version="1.0" encoding="UTF-8"?>
-<interface>
-  <object class="GtkShortcutsWindow" id="shortcuts_window">
-    <property name="modal">true</property>
-    <child>
-      <object class="GtkShortcutsSection">
-        <property name="title">General</property>
-        <property name="section-name">general</property>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">Application</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;n</property>
-                <property name="title">Open New Window</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;comma</property>
-                <property name="title">Open Preferences</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;question</property>
-                <property name="title">Keyboard Shortcuts</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">F1</property>
-                <property name="title">About Craic</property>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">Repository</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;p</property>
-                <property name="title">Pull remote changes</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;u</property>
-                <property name="title">Push local commits</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;r</property>
-                <property name="title">Refresh repository status</property>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">File Browser</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;c</property>
-                <property name="title">Copy selected file entries</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;x</property>
-                <property name="title">Cut selected file entries</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;v</property>
-                <property name="title">Paste file entries</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">Delete</property>
-                <property name="title">Delete selected file entry</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">Up</property>
-                <property name="title">Select previous entry</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">Down</property>
-                <property name="title">Select next entry</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">Return</property>
-                <property name="title">Open / expand selected folder</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">Escape</property>
-                <property name="title">Cancel file rename/new-entry mode</property>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">Code Editor</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;f</property>
-                <property name="title">Toggle search</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;a</property>
-                <property name="title">Select all</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;z</property>
-                <property name="title">Undo</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;&lt;Shift&gt;z</property>
-                <property name="title">Redo</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;y</property>
-                <property name="title">Redo</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;c</property>
-                <property name="title">Copy</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;Insert</property>
-                <property name="title">Copy</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;x</property>
-                <property name="title">Cut</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;v</property>
-                <property name="title">Paste</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;w</property>
-                <property name="title">Toggle word wrap</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;plus</property>
-                <property name="title">Increase font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;equal</property>
-                <property name="title">Increase font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Add</property>
-                <property name="title">Increase font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;minus</property>
-                <property name="title">Decrease font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;underscore</property>
-                <property name="title">Decrease font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Subtract</property>
-                <property name="title">Decrease font size</property>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">Terminal</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;c</property>
-                <property name="title">Copy terminal selection</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;&lt;Shift&gt;C</property>
-                <property name="title">Copy terminal selection</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;Insert</property>
-                <property name="title">Copy terminal selection</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;&lt;Shift&gt;v</property>
-                <property name="title">Paste terminal selection</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Shift&gt;Insert</property>
-                <property name="title">Paste terminal selection</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;BackSpace</property>
-                <property name="title">Delete previous word</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;plus</property>
-                <property name="title">Increase terminal font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;equal</property>
-                <property name="title">Increase terminal font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Add</property>
-                <property name="title">Increase terminal font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;minus</property>
-                <property name="title">Decrease terminal font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;underscore</property>
-                <property name="title">Decrease terminal font size</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Subtract</property>
-                <property name="title">Decrease terminal font size</property>
-              </object>
-            </child>
-          </object>
-        </child>
-        <child>
-          <object class="GtkShortcutsGroup">
-            <property name="title">PDF Preview</property>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;plus</property>
-                <property name="title">Zoom in</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;equal</property>
-                <property name="title">Zoom in</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Add</property>
-                <property name="title">Zoom in</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;minus</property>
-                <property name="title">Zoom out</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;underscore</property>
-                <property name="title">Zoom out</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;KP_Subtract</property>
-                <property name="title">Zoom out</property>
-              </object>
-            </child>
-            <child>
-              <object class="GtkShortcutsShortcut">
-                <property name="accelerator">&lt;Control&gt;c</property>
-                <property name="title">Copy selected text</property>
-              </object>
-            </child>
-          </object>
-        </child>
-      </object>
-    </child>
-  </object>
-</interface>
-"#;
+const SHORTCUT_SECTIONS: &[(&str, &[(&str, &str)])] = &[
+    (
+        "Application",
+        &[
+            ("Open New Window", "<Control>n"),
+            ("Open Preferences", "<Control>comma"),
+            ("Keyboard Shortcuts", "<Control>question"),
+            ("About Craic", "F1"),
+        ],
+    ),
+    (
+        "Repository",
+        &[
+            ("Pull remote changes", "<Control>p"),
+            ("Push local commits", "<Control>u"),
+            ("Refresh repository status", "<Control>r"),
+        ],
+    ),
+    (
+        "File Browser",
+        &[
+            ("Copy selected file entries", "<Control>c"),
+            ("Cut selected file entries", "<Control>x"),
+            ("Paste file entries", "<Control>v"),
+            ("Delete selected file entry", "Delete"),
+            ("Select previous entry", "Up"),
+            ("Select next entry", "Down"),
+            ("Open / expand selected folder", "Return"),
+            ("Cancel file rename/new-entry mode", "Escape"),
+        ],
+    ),
+    (
+        "Code Editor",
+        &[
+            ("Toggle search", "<Control>f"),
+            ("Select all", "<Control>a"),
+            ("Undo", "<Control>z"),
+            ("Redo", "<Control><Shift>z"),
+            ("Redo", "<Control>y"),
+            ("Copy", "<Control>c"),
+            ("Copy", "<Control>Insert"),
+            ("Cut", "<Control>x"),
+            ("Paste", "<Control>v"),
+            ("Toggle word wrap", "<Control>w"),
+            ("Increase font size", "<Control>plus"),
+            ("Increase font size", "<Control>equal"),
+            ("Increase font size", "<Control>KP_Add"),
+            ("Decrease font size", "<Control>minus"),
+            ("Decrease font size", "<Control>underscore"),
+            ("Decrease font size", "<Control>KP_Subtract"),
+        ],
+    ),
+    (
+        "Terminal",
+        &[
+            ("Copy terminal selection", "<Control>c"),
+            ("Copy terminal selection", "<Control><Shift>C"),
+            ("Copy terminal selection", "<Control>Insert"),
+            ("Paste terminal selection", "<Control><Shift>v"),
+            ("Paste terminal selection", "<Shift>Insert"),
+            ("Delete previous word", "<Control>BackSpace"),
+            ("Increase terminal font size", "<Control>plus"),
+            ("Increase terminal font size", "<Control>equal"),
+            ("Increase terminal font size", "<Control>KP_Add"),
+            ("Decrease terminal font size", "<Control>minus"),
+            ("Decrease terminal font size", "<Control>underscore"),
+            ("Decrease terminal font size", "<Control>KP_Subtract"),
+        ],
+    ),
+    (
+        "PDF Preview",
+        &[
+            ("Zoom in", "<Control>plus"),
+            ("Zoom in", "<Control>equal"),
+            ("Zoom in", "<Control>KP_Add"),
+            ("Zoom out", "<Control>minus"),
+            ("Zoom out", "<Control>underscore"),
+            ("Zoom out", "<Control>KP_Subtract"),
+            ("Copy selected text", "<Control>c"),
+        ],
+    ),
+];
 
-    let builder = gtk::Builder::from_string(xml);
-    log::debug!("Displaying keyboard shortcuts window");
-    if let Some(shortcuts_window) = builder.object::<gtk::ShortcutsWindow>("shortcuts_window") {
-        shortcuts_window.set_transient_for(Some(parent));
-        shortcuts_window.present();
+pub(super) fn show_shortcuts_window(parent: &adw::ApplicationWindow) {
+    let shortcuts_dialog = adw::ShortcutsDialog::builder()
+        .title("Keyboard Shortcuts")
+        .build();
+
+    for &(section_title, shortcuts) in SHORTCUT_SECTIONS {
+        let section = adw::ShortcutsSection::new(Some(section_title));
+
+        for &(shortcut_title, accelerator) in shortcuts {
+            section.add(adw::ShortcutsItem::new(shortcut_title, accelerator));
+        }
+
+        shortcuts_dialog.add(section);
     }
+
+    log::debug!("Displaying keyboard shortcuts dialog");
+    shortcuts_dialog.present(Some(parent));
 }
